491ffdb3f6ce0e4d193aa8bde62652a13a098fc1
Aleksandar Asenov <amasenov@outlook.com>
2022-12-30 19:07:59 +0200

    Remove obsolete configuration extensions

--- src/BuildingBlocks/Infrastructure/Extensions/ConfigurationExtensions.cs
+++ src/BuildingBlocks/Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -30,32 +30,6 @@ namespace ViaChatServer.BuildingBlocks.Infrastructure.Extensions
             return settingValue;
         }
 
-        public static int? GetIntSectionSetting(this IConfiguration configuration, string section, string settingName, bool required = true)
-        {
-            var settingValue = GetSectionSetting(configuration, section, settingName, required);
-
-            var validParsing = int.TryParse(settingValue, out int integerSettingValue);
-            if (!validParsing && required)
-            {
-                throw new ConfigurationException($"{section}__{settingName}");
-            }
-
-            return validParsing ? integerSettingValue : null;
-        }
-
-        public static Uri GetUriSectionSetting(this IConfiguration configuration, string section, string settingName, bool required = true)
-        {
-            var settingValue = GetSectionSetting(configuration, section, settingName, required);
-
-            var validParsing = Uri.TryCreate(settingValue, UriKind.RelativeOrAbsolute, out Uri result);
-            if (!validParsing && required)
-            {
-                throw new ConfigurationException($"{section}__{settingName}");
-            }
-            
-            return validParsing ? result : null;
-        }
-
         public static string GetSectionSetting(this IConfiguration configuration, string section, string settingName, bool required = true)
         {
             if (section.IsEmpty())
